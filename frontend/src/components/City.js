b32f32e26aa34fc4bd33c9b870f1738825066df0
EmaCuvillier <cuvillieremanuel7@gmail.com>
2021-06-08 18:18:52 -0300

    acomodo imagenes

--- frontend/src/components/City.js
+++ frontend/src/components/City.js
@@ -1,6 +1,6 @@
 const City = ({city}) =>{
     return (
-        <div className="blog-card" style={{backgroundImage:`url('./assets/${city.image}'`}}>
+        <div className="blog-card" style={{backgroundImage:`url('${city.image}`}}>
             <div className="title-content" >
                 <h3 className='nameCity'>{city.name}</h3>
                 <div className="intro"> <p>{city.country}</p> </div>
